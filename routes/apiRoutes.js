5ef7faba401fe5823a55c0f1df33b8a3b6c84e42
Farjana Huq <fa.huq03@gmail.com>
2019-04-22 21:06:31 -0700

    google-book-api works

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,62 @@
 const router = require('express').Router();
-//const axios = require(axios");
+const axios = require('axios');
 const db = require('../models');
+const JSON = require('circular-json');
 
 
+const ApiKey = "AIzaSyDrbGlYM7C1m_Dg0krbkxKKc1KknuA5Yhs";
+
+var url = `https://www.googleapis.com/books/v1/volumes?`
+// q=${query}&key=${ApiKey}
+// +inauthor:${inauthorKey}
+
+//search books
+router.get("/searchBooks/:query/:inauthorKey", (req, res) => {
+     const query = req.params.query;
+     const inauthorKey = req.params.inauthorKey;
+
+    axios.get( 
+        `https://www.googleapis.com/books/v1/volumes?q=${query}+inauthor:${inauthorKey}&key=${ApiKey}`
+    
+        //   `https://www.googleapis.com/books/v1/volumes?q=${query}&filter=free-ebooks&key=${ApiKey}`
+       
+        )
+      .then(
+          results => {
+            // res.json(results.data);
+            var resultArr = [];
+
+            for(var i=0; i<results.data.items.length; i++ ){
+
+                console.log("Link :", results.data.items[i].selfLink);
+                console.log("Title",  results.data.items[i].volumeInfo.title);
+                console.log("Description:", results.data.items[i].volumeInfo.description);
+                console.log("Thumbnail:",   results.data.items[i].volumeInfo.imageLinks.thumbnail);
+                console.log("===========================================================================")
+                resultArr.push({
+                      link: results.data.items[i].selfLink,
+                      title: results.data.items[i].volumeInfo.title,
+                      description: results.data.items[i].volumeInfo.description,
+                      thumbnail: results.data.items[i].volumeInfo.imageLinks.thumbnail
+                })
+            }
+            console.log("===========================================================================")
+            console.log("===========================================================================")
+            console.log(resultArr);
+            res.json(resultsArr);
+        })
+       
+      .catch(err => {
+        res.status(422).json({error: err});
+     });
+});
+  
+
+//displays all books
 router.get("/books", (req, res) => {
   db.Book.findAll({})
     .then(resp => { 
+        console.log(resp);
         res.json(resp);})
     .catch(function (err) {
         res.json(err);
@@ -13,6 +64,7 @@ router.get("/books", (req, res) => {
   
   });
 
+  // create new book
 router.post("/books", (req, res) => {
   
     db.Book.create({
@@ -31,7 +83,7 @@ router.post("/books", (req, res) => {
   
   });
 
-  // delete a company
+// deletes a book
 router.delete('/books/:id', function (req, res) {
 
     db.Book.remove( { _id : req.params.id})
